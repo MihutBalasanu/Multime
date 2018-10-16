94c26b934d9e843d2ebc5ce0b30d8943dd350002
MihutBalasanu <44089827+MihutBalasanu@users.noreply.github.com>
2018-10-16 16:37:30 +0300

    Add files via upload

--- 20181016_Multime/src/Multime.java
+++ 20181016_Multime/src/Multime.java
@@ -1,6 +1,6 @@
 public class Multime {
 
-    private int[]date;
+    private int[] date;
     private int dim;
     private int n;
 
@@ -17,26 +17,27 @@ public class Multime {
     }
 
     public void adauga(int a) {
-            if (n < dim) {
-                int i = 0;
-                while (date[i] != a && i < n) {
-                    i++;
-                }
-                if (i == n) {
-                    date[n] = a;
-                    n = n + 1;
-                }
-            } else {
-                System.out.println("Multimea nu mai accepta niciun element!");
-            
+
+        if (n < dim) {
+            int i = 0;
+            while (date[i] != a && i < n) {
+                i++;
+            }
+            if (i == n) {
+                date[n] = a;
+                n = n + 1;
+            }
+        } else {
+            System.out.println("Multimea nu mai accepta niciun element!");
         }
     }
 
+
     public void extrage(int a) {
         if (n != 0) {
-            for (int i = 0; i < n - 1; i++) {
+            for (int i = 0; i < n; i++) {
                 if (date[i] == a) {
-                    for (int j = i; j < n - 1; j++) {
+                    for (int j = i; j < n; j++) {
                         date[j] = date[j + 1];
                         date[n - 1] = 0;
                         n = n - 1;
@@ -46,23 +47,12 @@ public class Multime {
         }
     }
 
-    public void afisare(){
+    public void afisare() {
         for (int i = 0; i < n; i++) {
             System.out.print(date[i] + " ");
         }
-    }
-
-    public static void main(String[] args) {
-        Multime m = new Multime(10);
-        m.adauga(4);
-        m.adauga(3);
-        m.afisare();
-        m.extrage(4);
-        m.extrage(4);
-        m.afisare();
-        m.adauga(9);
-        m.adauga(2);
-        m.adauga(2);
-        m.afisare();
+        System.out.println();
     }
 }
+
+
